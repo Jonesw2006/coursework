f9bbf40eef098a47dd63cae72b3c0b61a6b494c6
Jonesw2006 <114936184+Jonesw2006@users.noreply.github.com>
2023-04-19 10:40:06 +0100

    Connection page

--- connection.php
+++ connection.php
@@ -1,5 +1,17 @@
 <?php
-//test
+//connection page
+$servername
+$username
+$password
+$dbname
 
-xcvx
+try {
+    $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
+
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    echo "Connected Successfully";
+
+    }
+
+
 ?>
