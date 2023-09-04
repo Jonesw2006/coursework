90fc0280769d5cbd756d8357366cfcbd3a6b8f38
Jonesw2006 <114936184+Jonesw2006@users.noreply.github.com>
2023-09-04 09:46:21 +0100

    bugs

---
+++ tutorprofile.php
@@ -0,0 +1,62 @@
+<!DOCTYPE HTML>
+<!--Declaring what document type this is -->
+<html>
+<head>
+
+    <title>Template</title>
+    <link rel="stylesheet" href="stylesheet.css" />
+</head>
+<body>
+    <nav class="navbar">
+
+    <div class="logo">TutorSpace</div>
+
+
+    <!-- creating the navigation bar-->
+    <ul class="navlinks">
+        <div class="menu">
+            <li><a href="home.php">Home</a></li>
+            <li><a href="search.php">Search</a></li>
+
+            <li class="tutors">
+                
+                <a href="tutorspace.php">Tutor Space</a>
+
+                
+            </li>
+
+        
+
+            
+            <!-- Sign up and login links -->
+            <li><a href="pupillogin.php">Login</a></li>
+            <li><a href="pupilsignup.php">Sign Up</a></li>
+        </div>
+    </ul>
+    </nav>
+
+
+    <div>
+
+
+
+    <div> 
+
+
+
+
+
+
+
+
+
+
+
+<footer>
+    <h4>Author: Will Jones │ Oundle School </h4>
+</footer>
+
+</body>
+
+
+</html>
